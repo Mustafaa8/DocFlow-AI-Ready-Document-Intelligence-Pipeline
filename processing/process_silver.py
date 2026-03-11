fe9a39d622fc48203f420de4c1e2a60836bcdd3c
Mustafaa8 <mostafa_abdallah27@outlook.com>
2026-03-11 02:36:21 +0200

    silver processing

--- processing/process_silver.py
+++ processing/process_silver.py
@@ -1,22 +1,47 @@
-import polars as pl
-from datetime import datetime as dt
 import os
+from datetime import datetime as dt
+
+import polars as pl
 from loguru import logger
+
 
 def load_json_files(date):
     filelist = os.listdir("./bronze")
-    filenames = [f"./bronze/{file}" for file in filelist if file.endswith(f"{date}.json")]
+    filenames = [
+        f"./bronze/{file}" for file in filelist if file.endswith(f"{date}.json")
+    ]
     df = pl.DataFrame()
     for file in filenames:
         file_df = pl.read_json(file)
-        df = pl.concat([df,file_df])
-    logger.info(df.shape)
+        df = pl.concat([df, file_df])
     return df
 
+
 def run_processing():
-    pass
+    fetching_date = dt.now().strftime("%Y-%m-%d")
+    df = load_json_files(fetching_date)
+    logger.info(f"Rows loaded : {df.shape[0]} row")
+    df = (
+        df.with_columns(
+            pl.col("title").str.strip_chars().str.replace_all(r"\s+", " "),
+            pl.col("abstract").str.strip_chars().str.replace_all(r"\s+", " "),
+            pl.col("published").str.slice(0, 10),
+            pl.col("abstract").str.split(" ").list.len().alias("abstract_word_count"),
+            pl.col("title").str.split(" ").list.len().alias("title_word_count"),
+        )
+        .with_columns(
+            pl.col("published").str.slice(0, 4).cast(pl.Int32).alias("publish_year"),
+            pl.col("published").str.slice(5, 2).cast(pl.Int32).alias("publish_month"),
+        )
+        .unique(subset=["id"])
+    )
+    logger.info(f"Rows after processing and deduplication : {df.shape[0]} row")
+    os.makedirs("silver", exist_ok=True)
+    df.write_parquet(f"./silver/arxiv_{fetching_date}.parquet")
+    logger.info(
+        f"Silver file has been written to ./silver/arxiv_{fetching_date}.parquet"
+    )
+
 
 if __name__ == "__main__":
-    fetching_date = dt.now().strftime("%Y-%m-%d")
-    load_json_files(fetching_date)
-    #run_processing()
+    run_processing()
